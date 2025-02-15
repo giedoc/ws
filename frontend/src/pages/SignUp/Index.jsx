a4990c180c1e4e85b6844a82fcb55641ce90caa3
giedoc <gokalpinan@outlook.com>
2025-02-15 15:23:24 +0000

    Sign Up

---
+++ frontend/src/pages/SignUp/Index.jsx
@@ -0,0 +1,96 @@
+import axios from "axios";
+import { useState } from "react";
+import { signUp } from "./api";
+
+export function SignUp() {
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [passwordRepeat, setPasswordRepeat] = useState();
+  const [apiProgress, setApiProgress] = useState(false);
+  const [successMessage, setSuccessMessage] = useState();
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setSuccessMessage();
+    setApiProgress(true);
+
+    try{
+      const response = await signUp({
+        username,
+        email,
+        password
+      })
+      setSuccessMessage(response.data.message)
+    } catch {
+
+    } finally {
+      setApiProgress(false)
+    }
+
+  }
+
+  return (
+    <div className="container">
+      <div className="col-lg-6 offset-lg-3 col-sm-8 offset-sm-2">
+        <form className="card" onSubmit={onSubmit}>
+          <div className="text-center card-header">
+            <h1> SignUp </h1>
+          </div>
+          <div className="card-body">
+            <div className="mb-3">
+              <label htmlFor="username" className="form-label">
+                Username
+              </label>
+              <input
+                htmlFor="username"
+                className="form-control"
+                onChange={(event) => setUsername(event.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="email" className="form-label">
+                E-Mail
+              </label>
+              <input
+                htmlFor="email"
+                className="form-control"
+                onChange={(event) => setEmail(event.target.value)}
+              ></input>
+            </div>
+            <div>
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                htmlFor="password"
+                className="form-control"
+                onChange={(event) => setPassword(event.target.value)}
+              ></input>
+            </div>
+            <div>
+              <label htmlFor="passwordRepeat" className="form-label">
+                Password Repeat
+              </label>
+              <input
+                htmlFor="passwordRepeat"
+                className="form-control"
+                onChange={(event) => setPasswordRepeat(event.target.value)}
+              ></input>
+            </div>
+            {successMessage && <div className="alert alert-success">{successMessage}</div>}
+            <div className="text-center">
+              <button
+                className="btn btn-primary"
+                disabled={apiProgress || (!password || password !== passwordRepeat)}
+              >
+                {apiProgress && <span className="spinner-border spinner-border-sm" aria-hidden="true"></span>}
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ src/main/java/com/hoaxify/ws/User/UserRepository.java
@@ -0,0 +1,8 @@
+package com.hoaxify.ws.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    
+}
